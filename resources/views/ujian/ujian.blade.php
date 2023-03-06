282253065f3e288dc230acf7c3c5bb8281e7d1d0
niken157 <nikencahyaning157@gmail.com>
2023-03-06 09:38:56 +0700

    p

--- resources/views/ujian/ujian.blade.php
+++ resources/views/ujian/ujian.blade.php
@@ -8,7 +8,7 @@
             <i class="fas fa-table me-1"></i>
             DATA KARTU UJIAN
             <span style="float: right">
-                <a class="align-items-center justify-content-between btn btn-warning" onclick="Semua()" role="button"><i class="fa-solid fa-print" title="cetak kartu semua peserta"></i> Cetak Kartu</a>
+                <a class="align-items-center justify-content-between btn btn-warning" href="/kartu" target="_blank" role="button"><i class="fa-solid fa-print" title="cetak kartu semua peserta"></i> Cetak Kartu</a>
 
             <a class="align-items-center justify-content-between btn btn-primary" href="/ujian/tambah" role="button"><i class="fas fa-fw fa-plus"></i> Tambah Data</a>
             <a class="align-items-center justify-content-between btn btn-primary" href="/ujian/generate" role="button"><i class="fas fa-fw fa-plus"></i> Generate</a>
@@ -36,18 +36,8 @@
                             <td>{{ $u->no_sesi}}</td>
                             <td>{{ $u->nomor_pc}}</td>
                             <td>
-                            <a class="btn btn-outline-warning" onclick="Satuan()"  href="/kartu_satuan/{{ $u->id_ujian }}" role="button" title="cetak kartu peserta"><i class="fa-solid fa-print"></i></a>
+                            <a class="btn btn-outline-warning" target="_blank"  href="/kartu_satuan/{{ $u->id_ujian }}" role="button" title="cetak kartu peserta"><i class="fa-solid fa-print"></i></a>
                                 <a class="btn btn-outline-primary" href="/ujian/edit/{{ $u->id_ujian }}" role="button" title="edit data kartu"><i class="fas fa-fw fa-edit"></i></a>
-                                <script>
-                                    function Satuan() {
-                                        window.open(
-                                        "/kartu_satuan/{{ $u->id_ujian }}", "_blank");
-                                    }
-                                    function Semua() {
-                                        window.open(
-                                        "/kartu", "_blank");
-                                    }
-                                </script>
                                 <a class="btn btn-outline-danger" href="/ujian/hapus/{{ $u->id_ujian }}" role="button" title="hapus data kartu"><i class="fas fa-fw fa-trash"></i></a>
                             </td>
                         </tr>
